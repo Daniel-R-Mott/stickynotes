66d626892699a16c05f50de14b5d207171cb08a5
Daniel-R-Mott <daniel.r.mott@gmail.com>
2024-10-03 11:02:27 -0700

    asdf

--- src/App.js
+++ src/App.js
@@ -3,36 +3,38 @@ import Header from "./Header.js";
 import NotesList from "./NotesList.js";
 
 class App extends Component {
-    state = {
-        notes: [
-            {
-                id: 0,
-                title: "eat",
-                description: "reese peanut butter cups",
-                doesMatchSearch: true
-            },
-            {
-                id: 1,
-                title: "sleep",
-                description: "eight hours",
-                doesMatchSearch: true
-            },
-            {
-                id: 2,
-                title: "code",
-                description: "build an awesome ui",
-                doesMatchSearch: true
-            }
-        ],
-        searchText: ""
-    }
-    render() {
-        <div>
-            <Header />
-            <NotesList />
-        </div>
-    }
+  state = {
+    notes: [
+      {
+        id: 0,
+        title: "eat",
+        description: "reese peanut butter cups",
+        doesMatchSearch: true,
+      },
+      {
+        id: 1,
+        title: "sleep",
+        description: "eight hours",
+        doesMatchSearch: true,
+      },
+      {
+        id: 2,
+        title: "code",
+        description: "build an awesome ui",
+        doesMatchSearch: true,
+      },
+    ],
+    searchText: "",
+  };
 
+  render() {
+    return (
+      <div>
+        <Header searchText={this.state.searchText} />
+        <NotesList notes={this.state.notes} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/NotesList.js
+++ src/NotesList.js
@@ -1,14 +1,10 @@
-import Note from "./Note.js"
+import Note from "./Note.js";
 import React from "react";
 
-const NotesList = () => {
-    return(
-        <ul className="notes-list">
-            <Note />
-            <Note />
-            <Note />
-        </ul>
-    )
-}
+const NotesList = (props) => {
+  const renderNote = (note) => <Note note={note} key={note.id} />;
+  const noteElements = props.notes.map(renderNote);
+  return <ul className="notes-list">{noteElements}</ul>;
+};
 
 export default NotesList;
