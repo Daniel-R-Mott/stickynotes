128e51e4e02f264e5d50b5828b5cb2a882da0ca7
Daniel-R-Mott <daniel.r.mott@gmail.com>
2024-10-04 12:07:32 -0700

    added search

--- src/App.js
+++ src/App.js
@@ -21,6 +21,28 @@ class App extends Component {
     this.setState({ notes: newNotes });
   };
 
+  onSearch = (text) => {
+    const newSearchText = text.toLowerCase();
+    const updatedNotes = this.state.notes.map((note) => {
+      if (!newSearchText) {
+        note.doesMatchSearch = true;
+        return note;
+      } else {
+        const title = note.title.toLowerCase();
+        const description = note.description.toLowerCase();
+        const titleMatch = title.includes(newSearchText);
+        const descriptionMatch = description.includes(newSearchText);
+        const hasMatch = titleMatch || descriptionMatch;
+        note.doesMatchSearch = hasMatch;
+        return note;
+      }
+    });
+    this.setState({
+      searchText: newSearchText,
+      notes: updatedNotes,
+    });
+  };
+
   onType = (editMeId, updatedKey, updatedValue) => {
     // editMeId == id of the note that is edited
     // updatedKey == title or description field
@@ -44,7 +66,11 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header searchText={this.state.searchText} addNote={this.addNote} />
+        <Header
+          searchText={this.state.searchText}
+          addNote={this.addNote}
+          onSearch={this.onSearch}
+        />
         <NotesList notes={this.state.notes} onType={this.onType} />
       </div>
     );
